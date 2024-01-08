1f0965748be57645a9ed98aa77337d3c485f1eda
Esteban <estebann.rubio@hotmail.com>
2024-01-08 00:40:33 -0300

    add Human.cs, eddit Program.cs
    Eddit program.cs with a few lines with the new class

---
+++ Human.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    public class Human
+    {
+        public int Year;
+        public int Month;
+        public int Day;
+
+        public string Name;
+        public float HeightCms;
+        public float WeightKgs;
+
+        
+    }
+}
